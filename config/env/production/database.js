2a9df0fffff52d44550df611bfde22b1d9492a3f
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-08-06 01:18:15 +0530

    ...

---
+++ config/env/production/database.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "mysql2",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        ca: fs
+          .readFileSync(path.join(__dirname, "./ca-certificate.crt"))
+          .toString(),
+      },
+    },
+  },
+});
